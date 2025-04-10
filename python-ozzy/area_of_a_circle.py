579dad5a5266870c64108edbe80c906ae2741531
mathew kiwanuka <kiwamathew6@gmail.com>
2025-04-10 21:05:53 +0300

    added a program that computes the area of a circle

---
+++ python-ozzy/area_of_a_circle.py
@@ -0,0 +1,11 @@
+# print("Twinkle, twinkle, little star,\n"
+#       +" "+" "+" "+" "+ "How I wonder what you are!\n" 
+#       +" "+" "+" "+" "+ "Up above the world so high,\n" 
+#       +" "+" "+" "+" "+ "Like a diamond in the sky.\n" 
+#       "Twinkle, twinkle, little star, How I wonder what you are")
+
+
+import math
+r = float(input("Enter the radius of the circle: "))
+area = (math.pi)*(r**2)
+print(area)
