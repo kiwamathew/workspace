ab4064a36e4c32b61d3a6355d0a4988a94e51030
mathew kiwanuka <kiwamathew6@gmail.com>
2025-04-10 23:51:45 +0300

    added a program that calculates the highest and lowest numbers in a list without using in-built functions

--- python-ozzy/area_of_a_circle.py
+++ python-ozzy/area_of_a_circle.py
@@ -5,7 +5,27 @@
 #       "Twinkle, twinkle, little star, How I wonder what you are")
 
 
-import math
-r = float(input("Enter the radius of the circle: "))
-area = (math.pi)*(r**2)
-print(area)
+# import math
+# r = float(input("Enter the radius of the circle: "))
+# area = (math.pi)*(r**2)
+# print(area)
+
+# name = "Kiwanuka Mathew"
+# reversed_name = name[::-1]
+# print(reversed_name)
+
+
+# x = int(input("Enter number: "))
+# m = str(x)*3
+# n = str(x)*2
+# o = str(x)
+# value = int(m)+int(n)+int(o)
+# print(value)
+
+# # A program that finds the maximum and minimum without built-ins
+# my_list = [34, 12, 56, 17, 4,  -1, 88] 
+# my_list.sort()
+# print(my_list)
+# lowest = my_list[0]
+# highest = my_list[-1]
+# print(f"Lowest number: {lowest}\n Highest number: {highest}")
---
+++ python-ozzy/min_max.py
@@ -0,0 +1,8 @@
+# A program that finds the maximum and minimum without built-ins
+
+my_list = [34, 12, 56, 17, 4,  -1, 88] 
+my_list.sort()
+print(my_list)
+lowest = my_list[0]
+highest = my_list[-1]
+print(f"Lowest number: {lowest}\n Highest number: {highest}")
