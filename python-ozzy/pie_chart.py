ba7ffb6fe6d8bf6d73b4be9fc430728839b90997
mathew kiwanuka <kiwamathew6@gmail.com>
2025-04-11 20:05:54 +0300

    added a program that draws a piechart and then commented it out

---
+++ python-ozzy/pie_chart.py
@@ -0,0 +1,6 @@
+# from matplotlib import pyplot as plt
+# labels = ['Python', 'Java', 'HTML', 'C++', 'Javascript']
+# data = [95, 80, 65, 80, 95]
+# explode = [0.0, 0.0, 0.1, 0.0, 0.0]
+# plt.pie(data, labels=labels, explode=explode)
+# plt.show()
