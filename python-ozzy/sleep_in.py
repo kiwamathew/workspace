5e20c2855b050040687d6d9b53a2db89733b99ca
mathew kiwanuka <kiwamathew6@gmail.com>
2025-04-15 00:16:09 +0300

    changed the if conditionals to strings by adding quotes

--- python-ozzy/sleep_in.py
+++ python-ozzy/sleep_in.py
@@ -1,12 +1,17 @@
 def sleep_in(weekday, vacation):
-  if weekday == False and vacation == False:
-    print(True)
-  elif weekday == True and vacation == False:
-      print(False)
-  elif weekday == False and vacation == True:
-      print(True)
+    if weekday == 'False' and vacation == 'False':
+        print(True)
+    elif weekday == 'True' and vacation == 'False':
+        print(False)
+    elif weekday == 'False' and vacation == 'True':
+        print(True)
+
+    return 
   
 weekday = input("Enter true or false: ")
 vacation = input("Enter true or false: ")
 
 print(sleep_in(weekday, vacation))
+
+
+
